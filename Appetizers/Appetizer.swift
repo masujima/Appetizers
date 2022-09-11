09cdc404904f35cc0db053b6d37a69f2d57f7024
masujima <masujima.ernar@gmail.com>
2022-09-11 15:57:27 +0600

    Create model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,28 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Yernar Masujima on 9/11/22.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 1, name: "Test", description: "Test Test", price: 10, imageURL: "", calories: 4, protein: 8, carbs: 5)
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
