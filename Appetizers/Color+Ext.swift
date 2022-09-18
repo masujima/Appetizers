1a4fdb6d8ad65608247d5ba4bb31b27e3d06ccc0
masujima <masujima.ernar@gmail.com>
2022-09-18 20:42:00 +0600

    Add color extension

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Yernar Masujima on 9/18/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
